cb35ce7ae474ed4aa4a4980565890b9d89a05575
nicklewis810 <nicklewis810@gmail.com>
2019-08-18 02:33:54 -0400

    initial commit

---
+++ src/components/AlertBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-carousel cycle height="50" hide-delimiters :show-arrows="false">
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div id="text" class="display-2">{{ slide }}</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ["success", "primary", "red lighten-1"],
+      slides: [
+        "ATTENTION PLAYERS! - SEASON 1 BEGINS ON SEPTEMBER 20TH, 2019",
+        "CONGRATULATIONS TO THE DONG STOMPERS ON WINNING THE SEASON ONE SPL PREMIERE LEAGUE CHAMPIONSHIP!!!",
+        "ATTENTION PLAYERS! - SIGNUPS FOR SEASON 2 WILL LOCK ON JANUARY 2ND 2019 AT 8PM"
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.v-application .display-2 {
+  font-size: 0.95rem !important;
+  font-weight: 400;
+  line-height: 3.125rem;
+  letter-spacing: normal !important;
+  font-family: 'Roboto', sans-serif !important;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-toolbar>
+      <div class="text-center">
+        <v-avatar :tile="true">
+          <img
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          />
+        </v-avatar>
+      </div>
+      <v-toolbar-title>SUPER PREMIERE LEAGUE</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>OVERVIEW</v-btn>
+        <v-btn text>SCHEDULE</v-btn>
+        <v-btn text>STANDINGS</v-btn>
+        <v-btn text>TEAMS</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <div class="my-2">
+          <v-btn small color="primary">Sign-Up</v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn small color="success">Log-In</v-btn>
+        </div>
+      </template>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.v-btn:not(.v-btn--round).v-size--small {
+  margin-left: 10px;
+}
+
+.v-toolbar__items {
+  margin-right: 10px;
+}
+</style>
