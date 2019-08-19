17c53849d6dda1e4b0459c3fa58a1edae9390346
drewhust <andhust@gmail.com>
2019-08-19 16:09:17 -0600

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <v-toolbar>
-      <div class="text-center">
-        <v-avatar :tile="true">
-          <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar" />
-        </v-avatar>
-      </div>
+    <v-app-bar color="blue-grey darken-4" dark>
+      <v-app-bar-nav-icon v-if="$vuetify.breakpoint.mdAndDown" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-avatar :tile="true">
+        <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar" />
+      </v-avatar>
       <v-toolbar-title>SUPER PREMIERE LEAGUE</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-toolbar-items>
+      <v-toolbar-items v-if="$vuetify.breakpoint.lgAndUp">
         <v-btn text to="/overview">OVERVIEW</v-btn>
         <v-btn text to="/scores">SCORES</v-btn>
         <v-btn text to="/schedule">SCHEDULE</v-btn>
@@ -18,15 +17,33 @@
         <v-btn text to="/teams">TEAMS</v-btn>
       </v-toolbar-items>
 
-      <template v-if="$vuetify.breakpoint.smAndUp">
-        <div class="my-2">
-          <v-btn small color="primary" @click.stop="showSignupDialog=true">Sign-Up</v-btn>
-        </div>
-        <div class="my-2">
-          <v-btn small color="success" @click.stop="showLoginDialog=true">Log-In</v-btn>
-        </div>
+      <template v-if="$vuetify.breakpoint.mdAndUp">
+        <v-btn small color="primary" @click.stop="showSignupDialog=true">Sign-Up</v-btn>
+        <v-btn small color="success" @click.stop="showLoginDialog=true">Login</v-btn>
       </template>
-    </v-toolbar>
+
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list dense>
+        <v-list-item>
+          <v-btn text to="/overview">OVERVIEW</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text to="/scores">SCORES</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text to="/schedule">SCHEDULE</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text to="/standings">STANDINGS</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text to="/teams">TEAMS</v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <LoginDialog v-model="showLoginDialog" />
     <SignupDialog v-model="showSignupDialog" />
   </div>
@@ -45,7 +62,8 @@ export default {
   data() {
     return {
       showLoginDialog: false,
-      showSignupDialog: false
+      showSignupDialog: false,
+      drawer: null,
     };
   }
 };
