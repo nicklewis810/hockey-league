f344754b9040d977075475458d8d8126f8efaede
nicklewis810 <nicklewis810@gmail.com>
2019-08-27 21:23:17 -0400

    added firebase imports and json config changes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 Vue.config.productionTip = false;
 
