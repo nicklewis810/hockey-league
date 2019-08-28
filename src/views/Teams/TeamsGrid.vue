691f79b1eb90620848c6e57d6c7b924a9f25d69f
nicklewis810 <nicklewis810@gmail.com>
2019-08-27 23:24:31 -0400

    Update TeamsGrid.vue

--- src/views/Teams/TeamsGrid.vue
+++ src/views/Teams/TeamsGrid.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <v-container>
-      <v-row justify="center" justify-sm="center" justify-md="start" justify-lg="start">
-        <v-col xl="2" lg="3" md="4" sm="5" cols="9">
-          <v-hover
-            v-slot:default="{ hover }"
-            :open-delay="openDelay"
-            :close-delay="closeDelay"
-            :disabled="disabled"
-            :value="value"
-          >
-            <v-card :elevation="hover ? 12 : 2">
-              <v-card-title class="justify-center">TORONTO TURTLES</v-card-title>
+      <v-row
+        justify="center"
+        justify-sm="center"
+        justify-md="start"
+        justify-lg="start"
+      >
+        <div v-for="team in teams" v-bind:key="team.id">
+          <v-col xl="3" lg="4" md="4" sm="6" cols="9">
+            <v-card>
+              <v-card-title class="justify-center">
+                {{ team.name }}
+              </v-card-title>
               <v-divider></v-divider>
               <div class="text-center">
                 <v-avatar size="75">
@@ -24,15 +25,43 @@
                 <v-icon small>mdi-arrow-right</v-icon>
               </v-card-actions>
             </v-card>
-          </v-hover>
-        </v-col>
+          </v-col>
+        </div>
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+import db from "@/firebase/init";
+
+export default {
+  data() {
+    return {
+      teams: []
+    };
+  },
+
+  created() {
+    //fetch data from firestore
+    db.collection("teams")
+      .get()
+      // db.collection grabs all information from a firestore collection
+      .then(snapshot => {
+        //snapshot views the collection and all of its dcocuments at this moment in time
+        snapshot.forEach(doc => {
+          //cycles through the snapshot documents using for each loop
+          console.log(doc.data(), doc.id);
+          //doc.data pulls the actual values wthin that document
+          //doc.id pulls the id of the document
+          //Doc.id is a separate call because id is not a value of the document but rather a property of the docuement itself
+          let team = doc.data();
+          team.id = doc.id;
+          this.teams.push(team);
+        });
+      });
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
