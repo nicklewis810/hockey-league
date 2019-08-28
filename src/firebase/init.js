6db1f0aff85ebfaf66d89fd706b8dac3fbb75e31
nicklewis810 <nicklewis810@gmail.com>
2019-08-27 21:48:48 -0400

    setup firebase config file
    to include auth and firestore

---
+++ src/firebase/init.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBlWaN7ppZGS56SudlLRuRcJwONATWkPyk",
+  authDomain: "hockey-league-aa693.firebaseapp.com",
+  databaseURL: "https://hockey-league-aa693.firebaseio.com",
+  projectId: "hockey-league-aa693",
+  storageBucket: "hockey-league-aa693.appspot.com",
+  messagingSenderId: "287660371506",
+  appId: "1:287660371506:web:c34cc864d7752c14"
+};
+// Initialize Firebase
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+firebaseapp.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebaseapp.firestore();
--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
 
 Vue.config.productionTip = false;
 
