c4e1fff162e859d9f2e8fd2a5220b50eded07a8f
Nick Lewis <nicklewis810@gmail.com>
2019-08-28 19:28:36 -0400

    Update TeamsGrid.vue

--- src/views/Teams/TeamsGrid.vue
+++ src/views/Teams/TeamsGrid.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
     <v-container>
-      <v-row
-        justify="center"
-        justify-sm="center"
-        justify-md="start"
-        justify-lg="start"
-      >
-        <div v-for="team in teams" v-bind:key="team.id">
-          <v-col xl="3" lg="4" md="4" sm="6" cols="9">
-            <v-card>
-              <v-card-title class="justify-center">
-                {{ team.name }}
-              </v-card-title>
-              <v-divider></v-divider>
-              <div class="text-center">
-                <v-avatar size="75">
-                  <img src="~@/assets/logo_turtles.png" alt="No Image Found" />
-                </v-avatar>
-              </div>
-              <v-divider></v-divider>
-              <v-card-actions class="justify-center">
-                <v-btn text>View Team</v-btn>
-                <v-icon small>mdi-arrow-right</v-icon>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </div>
+      <v-row justify="center" justify-sm="center" justify-md="start" justify-lg="start">
+        <v-col v-for="team in teams" v-bind:key="team.id" xl="2" lg="3" md="4" sm="6" cols="9">
+          <v-card>
+            <v-card-title class="justify-center">{{ team.name }}</v-card-title>
+            <v-divider></v-divider>
+            <div class="text-center">
+              <v-avatar size="75">
+                <img src="~@/assets/logo_turtles.png" alt="No Image Found" />
+              </v-avatar>
+            </div>
+            <v-divider></v-divider>
+            <v-card-actions class="justify-center">
+              <v-btn text>View Team</v-btn>
+              <v-icon small>mdi-arrow-right</v-icon>
+            </v-card-actions>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
